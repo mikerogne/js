22e6af0afa51c7b9353f464671d5d01f26dbff2e
Mike Rogne <mike@infinitywebcreations.com>
2016-07-02 22:30:53 -0500

    Exercise: Bean Counting

---
+++ eloquent/Ch3/bean_counting.js
@@ -0,0 +1,34 @@
+/*
+ You can get the Nth character, or letter, from a string by writing "string".charAt(N),
+ similar to how you get its length with "s".length. The returned value will be a string
+ containing only one character (for example, "b"). The first character has position zero,
+ which causes the last one to be found at position string.length - 1.
+
+ In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+
+ Write a function countBs that takes a string as its only argument and returns a number that indicates
+ how many uppercase “B” characters are in the string.
+
+ Next, write a function called countChar that behaves like countBs, except it takes a second argument
+ that indicates the character that is to be counted (rather than counting only uppercase “B” characters).
+ Rewrite countBs to make use of this new function.
+ */
+
+function countBs(s) {
+    return countChar(s, 'B');
+}
+
+function countChar(s, char) {
+    var i, total = 0, length = s.length;
+
+    for (i = 0; i < length; i++) {
+        if (s.charAt(i) == char) { total++; } // or s[i] instead of s.charAt(i).
+    }
+
+    return total;
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
