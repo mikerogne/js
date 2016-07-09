dd90c939b559955c7a134f65a50782f852217131
Mike Rogne <mike@infinitywebcreations.com>
2016-07-09 12:32:00 -0500

    Slight modification

--- data-structures/Chapter2/fibonacci.js
+++ data-structures/Chapter2/fibonacci.js
@@ -7,6 +7,6 @@ for (var i = 3; i < 20; i++) {
     fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 }
 
-fibonacci.forEach(function(el) {
-    console.log(el);
+fibonacci.forEach(function (value, index) {
+    console.log(value);
 });
