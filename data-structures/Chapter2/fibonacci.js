b32e9d7ef7d4a0fc6230c4455f30aa8eca8c94e8
Mike Rogne <mike@infinitywebcreations.com>
2016-07-09 12:33:35 -0500

    Add description

--- data-structures/Chapter2/fibonacci.js
+++ data-structures/Chapter2/fibonacci.js
@@ -1,3 +1,8 @@
+/*
+ First two numbers in a fibonacci sequence are 1. After that, each value is the sum of the previous two numbers.
+ For example, [1, 1, 2, 3, 5, 8 ...]
+ Because 1+1=2, 2+1=3, 3+2=5, 5+3=8, ...
+ */
 var fibonacci = [];
 
 fibonacci[1] = 1;
