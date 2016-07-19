4b8ebfa65efe688bf192b05744b8a6ad335566f5
Mike Rogne <mike@infinitywebcreations.com>
2016-07-19 18:01:30 -0500

    Finished with watcher-argv.js

--- nodejs-therightway/Chapter2/watcher-argv.js
+++ nodejs-therightway/Chapter2/watcher-argv.js
@@ -1,10 +1,23 @@
+'use strict';
+
 const fs       = require('fs');
+const spawn    = require('child_process').spawn;
 const filename = process.argv[2] || 'target.txt';
 
 // For some reason, fs.watch() wouldn't work after the first time it reported a change.
 // fs.watchFile() does work, however.
 fs.watchFile(filename, function () {
-    console.log(`File '${filename}' just changed!`);
+    let ls     = spawn('ls', ['-lh', filename]);
+    let output = '';
+
+    ls.stdout.on('data', function (chunk) {
+        output += chunk.toString();
+    });
+
+    ls.on('close', function () {
+        let parts = output.split(/\s+/);
+        console.dir([parts[0], parts[4], parts[8]]);
+    });
 });
 
 console.log(`Now watching '${filename}' for changes...`);
